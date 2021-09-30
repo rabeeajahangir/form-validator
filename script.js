9eeb5614c3816a5add93da455b958574f9964e38
rabeea jahangir <rabeeajahangir@gmail.com>
2021-09-30 00:21:52 -0400

    check required and refactor

--- script.js
+++ script.js
@@ -27,38 +27,29 @@ function isValidEmail (email) {
 }
 
 
+    //Check Required Fields
+    function checkRequired (inputArray) {
+
+    inputArray.forEach(function(input) {
+        if(input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    
+});
+
+//Get Field Name
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 //Event Listeners
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-if (username.value === '') {
-    showError('Username is required');
-} else {
-    showSuccess(username);
-}
 
+checkRequired([username, email, password, password2]);
 
-if (email.value === '') {
-    showError('email is required');
-    
-} else if (!isValidEmail(email.value)){
-    showError('email is required');
-} else {
-    showSuccess(email);
-}
-
-
-if (password.value === '') {
-    showError('Password is required');
-} else {
-    showSuccess(password);
-}
-
-
-if (password2.value === '') {
-    showError('Password 2 is required');
-} else {
-    showSuccess(password2);
-}
 
 });
